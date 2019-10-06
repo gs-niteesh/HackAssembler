6018d0836fc01c2a653fc24f7b1f86dd61b0b50b
gs-niteesh <gsnb.gn@gmail.com>
2019-10-06 13:48:48 +0530

    Added predefined symbols to the symbol table

---
+++ src/SymbolTable.cpp
@@ -0,0 +1,28 @@
+#include "SymbolTable.h"
+
+SymbolTable::SymbolTable(std::fstream &stream) : stream(stream)
+{
+    symbols.insert({"R0", "0"});
+    symbols.insert({"R1", "1"});
+    symbols.insert({"R2", "2"});
+    symbols.insert({"R3", "3"});
+    symbols.insert({"R4", "4"});
+    symbols.insert({"R5", "5"});
+    symbols.insert({"R6", "6"});
+    symbols.insert({"R7", "7"});
+    symbols.insert({"R8", "8"});
+    symbols.insert({"R9", "9"});
+    symbols.insert({"R10", "10"});
+    symbols.insert({"R11", "11"});
+    symbols.insert({"R12", "12"});
+    symbols.insert({"R13", "13"});
+    symbols.insert({"R14", "14"});
+    symbols.insert({"R15", "15"});
+    symbols.insert({"SP", "0"});
+    symbols.insert({"LCL", "1"});
+    symbols.insert({"ARG", "2"});
+    symbols.insert({"THIS", "3"});
+    symbols.insert({"THAT", "4"});
+    symbols.insert({"SCREEN", "16384"});
+    symbols.insert({"KBD", "24576"});
+}
