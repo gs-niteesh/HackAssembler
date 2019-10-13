03c9cce51a5b9f4891e4dee73fe0c80400c82aaa
gs-niteesh <gsnb.gn@gmail.com>
2019-10-13 13:45:01 +0530

    Changed variable name and added generate function

--- include/CodeGenerator.h
+++ include/CodeGenerator.h
@@ -1,12 +1,14 @@
 #pragma once
 #include "header.h"
+#include "SymbolTable.h"
 
 class CodeGenerator
 {
 private:
-    const SymbolTable labels; 
-    std::fstream output;
+    const SymbolTable &symbols; 
+    std::fstream &output;
 public:
     CodeGenerator(const SymbolTable &, std::fstream &);
+    void generate(INSTRUCTION &);
     ~CodeGenerator();
 };
